bd56ea31c05cf0034020b9098b16cf1dcba584c1
Andrea Massetani <andrea.massetani.stud@tulliobuzzi.edu.it>
2025-06-24 16:35:49 +0200

    Make square root

--- esercizio.go
+++ esercizio.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for i, prev := 0, z; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		if prev == z {
+			return z
+		}
+		fmt.Println(x, z)
+		prev = z
+	}
+
+	return z
+}
+
 func main() {
-	fmt.Println("Hello world")
+	for i := 1; i < 10; i++ {
+		fmt.Println(Sqrt(float64(i)))
+	}
+
 }
